9c8c7bb17d88351c803148253f82e9ece8ebaa66
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-16 22:51:43 +0200

    1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,23 +1,23 @@
-import css from "./TransactionHistory.css";
+import css from "./TransactionHistory.module.css";
 import transactions from "../../transactions.json";
 
 const TransactionHistory = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={css.table}>
+      <thead className={css.thead}>
+        <tr className={css.trHead}>
+          <th className={css.thHead}>Type</th>
+          <th className={css.thHead}>Amount</th>
+          <th className={css.thHead}>Currency</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className={css.tbody}>
         {transactions.map(({ id, type, amount, currency }) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+            <tr key={id} className={css.trBody}>
+              <td className={css.tdBody}>{type}</td>
+              <td className={css.tdBody}>{amount}</td>
+              <td className={css.tdBody}>{currency}</td>
             </tr>
           );
         })}
